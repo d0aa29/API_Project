1ace836419991e1fb6f8180a5e1be5102e81fcc2
doaa <doaazakaria29@gmail.com>
2024-09-09 00:17:37 +0300

    Adding IReporitory and Repository

---
+++ MyAPI/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyAPI.Models;
+using System.Linq.Expressions;
+
+namespace MyAPI.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll(Expression<Func<T, bool>>? fillter = null);
+
+        Task<T> Get(Expression<Func<T, bool>> fillter = null, bool tracked = true);
+        Task Create(T villa);
+       
+        Task Remove(T villa);
+        Task Save();
+    }
+}
---
+++ MyAPI/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MyAPI.Data;
+using MyAPI.Models;
+using MyAPI.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MyAPI.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+        internal DbSet<T> dbSet { get; set; }
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet=_db.Set<T>();
+         //   _mapper = mapper;
+        }
+
+        public async Task Create(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(T entity )
+        {
+            dbSet.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+       
+    }
+}
