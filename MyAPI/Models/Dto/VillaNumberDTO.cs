83238a3ce623022a8d3b6489d86ec316e285bd7a
doaa <doaazakaria29@gmail.com>
2024-09-09 03:27:18 +0300

    create VillaNumberDTO

---
+++ MyAPI/Models/Dto/VillaNumberDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyAPI.Models.Dto
+{
+	public class VillaNumberDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public int VillaNo { get; set; }
+
+        public string SpecialDetails { get; set; }
+    }
+}
