804ac47f28579f9fc21842c6c0ad01d34301d23c
doaa <doaazakaria29@gmail.com>
2024-09-11 21:47:47 +0300

    adding UserRepository model

---
+++ MyAPI/Repository/UserRepository.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using MyAPI.Data;
+using MyAPI.Models;
+using MyAPI.Models.Dto;
+using MyAPI.Repository.IRepository;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+        private string secretKey;
+
+        public UserRepository(ApplicationDbContext db, IConfiguration configuration)
+        {
+
+            _db = db;
+            secretKey = configuration.GetValue<string>("JWT:Secret");
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+           if( _db.LocalUsers.FirstOrDefault(u => u.UserName == username)==null)
+                return true;
+           return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+           var user= _db.LocalUsers.FirstOrDefault(
+             u=>u.UserName.ToLower()== loginRequestDTO.UserName.ToLower()&& u.Password== loginRequestDTO.Password) ;
+            if (user == null)
+            {
+                return new LoginResponseDTO()
+                {
+                    Token = "",
+                    User = null
+                };
+            }
+            //JWT
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user,
+
+            };
+            return loginResponseDTO;
+        }
+
+        public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            LocalUser user = new ()
+            {
+               UserName= registerationRequestDTO.UserName,
+               Name= registerationRequestDTO.Name,
+               Role = registerationRequestDTO.Role,
+               Password = registerationRequestDTO.Password
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
