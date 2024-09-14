513f36f600d8e471f43ce6bbcf9aea138124db46
doaa <doaazakaria29@gmail.com>
2024-09-14 07:34:36 +0300

    adding UserDTO

---
+++ MyAPI/Models/Dto/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAPI.Models.Dto
+{
+    public class UserDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
